d90031b5d7b4e84a4b84b5c8cda42df75f126a71
MuratSarsembayev <52274981+MuratSarsembayev@users.noreply.github.com>
2022-06-14 15:08:53 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 import pygsheets
 
-bot_token = "5246307613:AAEl-YRYmzyGcnqynUfZwjU-eP_agnTfNqY"
+bot_token = ""
 
-tg_bot_database = "db_tg_bot"
-tg_bot_user = "tg_bot_raw_user"
-tg_bot_password = "%9d[Z8c=J}Zc"
-tg_bot_host = "127.0.0.1"
-tg_bot_port = "5432"
+tg_bot_database = ""
+tg_bot_user = ""
+tg_bot_password = ""
+tg_bot_host = ""
+tg_bot_port = ""
 
-eventlog_database = "db_eventlog"
-eventlog_user = "postgres"
-eventlog_password = "Ac3r753614"
-eventlog_host = "127.0.0.1"
-eventlog_port = "5432"
+eventlog_database = ""
+eventlog_user = ""
+eventlog_password = ""
+eventlog_host = ""
+eventlog_port = ""
 
-client = pygsheets.authorize(service_account_file='dark-pipe-338808-d5b8d22ab578.json')
-sheet = client.open_by_key('1D-gMdwuzN2dTGOGBbt74RAKtMCD_ZvauGQ4_D3AAET4')
+client = pygsheets.authorize(service_account_file=)
+sheet = client.open_by_key()
 work_sheet = sheet.worksheet_by_title('customer_numbers')
